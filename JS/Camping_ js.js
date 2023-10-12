0eb73448dffe35ddf2fae22b8a9f84cac59caa6e
zawyehtet <scottsimon4969@gmail.com>
2023-10-13 02:37:54 +0630

    thaw thaw assigment yay pay tar pr

---
+++ JS/Camping_ js.js
@@ -0,0 +1,6 @@
+// // const menu = document.querySelector('#menu-btn');
+// const navlist = document.querySelector('.navbar');
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navlist.classList.toggle('open');
+// //
